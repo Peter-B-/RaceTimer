d66d78e1f538945f84c6f7c2aa71135f261641e7
Peter Butzhammer <peter@butzhammer.de>
2020-11-27 20:27:58 +0100

    Create RaceInfo struct

--- RaceTimer.ino
+++ RaceTimer.ino
@@ -3,39 +3,95 @@
 const int rs = 12, en = 11, d4 = 6, d5 = 5, d6 = 4, d7 = 3;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
+uint8_t PinLapA = A1;
+uint8_t PinLapB = A2;
+uint8_t PinReset = A3;
+
+struct RaceInfo
+{
+  bool Started;
+  unsigned long LapStart;
+  int CurrentLap;
+  unsigned long LastLapTime;
+  int BestLap;
+  unsigned long BestLapTime;
+};
+
+struct RaceInfo A = {0};
+struct RaceInfo B = {0};
+
 void setup()
 {
+  Reset(&A);
+  Reset(&B);
+
+  pinMode(PinLapA, INPUT_PULLUP);
+  pinMode(PinLapB, INPUT_PULLUP);
+  pinMode(PinReset, INPUT_PULLUP);
+
   lcd.begin(20, 4);
   lcd.clear();
+
+  Serial.begin(112500);
 }
 
 void loop()
 {
+  if (digitalRead(PinReset) == LOW)
+  {
+    Reset(&A);
+    Reset(&B);
+  }
 
-  auto time = millis();
+  if (digitalRead(PinLapA) == LOW)
+   SetLap(&A);
+
+  if (digitalRead(PinLapB) == LOW)
+    SetLap(&B);
+
+  auto now = millis();
+
   lcd.setCursor(0, 0);
-  PrintTime(time, 0);
-
-  lcd.setCursor(10, 0);
-  PrintTime(time, 1);
-
+  lcd.print("24 123.45  25  17,23");
   lcd.setCursor(0, 1);
-  PrintTime(time, 2);
+  lcd.print("12 111.10   9  17,23");
+}
 
-  lcd.setCursor(10, 1);
-  PrintTime(time, 3);
+void Reset(RaceInfo *info)
+{
+  info->Started = false;
+  info->LapStart = 0;
+  info->CurrentLap = 1;
+  info->LastLapTime = 0;
+  info->BestLap = 0;
+  info->BestLapTime = 0;
+}
 
-  lcd.setCursor(0, 2);
-  PrintTime(time * 7, 1);
+void SetLap(RaceInfo *info)
+{
+  auto now = millis();
+  
+  if (!info->Started)
+  {
+    info->Started = true;
+    info->LapStart = now;
+    return;
+  }
 
-  lcd.setCursor(10, 2);
-  PrintTime(time * 7, 3);
+  // Debounce
+  if (now - info->LapStart < 100) 
+    return; 
 
-  lcd.setCursor(0, 3);
-  PrintTime(time * 21, 1);
+  auto lapTime = now - info->LapStart;
 
-  lcd.setCursor(10, 3);
-  PrintTime(time * 21, 2);
+  info->LastLapTime = lapTime;
+  if (lapTime < info->BestLapTime)
+  {
+    info->BestLap = info->CurrentLap;
+    info->BestLapTime = lapTime;
+  }
+  info->LapStart = now;
+  info->CurrentLap++;
 }
 
 void PrintTime(unsigned long ms)
