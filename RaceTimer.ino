e603c6eada3c113fef18387f366c5c1b4564e4aa
Peter Butzhammer <peter.butzhammer@evopro-ag.de>
2020-11-27 22:36:07 +0100

    Implement remaining logic

--- RaceTimer.ino
+++ RaceTimer.ino
@@ -7,18 +7,33 @@ uint8_t PinLapA = A1;
 uint8_t PinLapB = A2;
 uint8_t PinReset = A3;
 
+byte SymbolAverage[8] = {
+  0b00000,
+  0b00001,
+  0b01110,
+  0b10011,
+  0b10101,
+  0b11001,
+  0b01110,
+  0b10000,
+};
+
 struct RaceInfo
 {
   bool Started;
   unsigned long LapStart;
   int CurrentLap;
-  unsigned long LastLapTime;
+  unsigned long LastTime;
   int BestLap;
-  unsigned long BestLapTime;
+  unsigned long BestTime;
+  bool LapUpdate;
+  unsigned long AverageTime;
+  unsigned long SumTime;
 };
 
 struct RaceInfo A = {0};
 struct RaceInfo B = {0};
+bool isReset = true;
 
 void setup()
 {
@@ -30,8 +45,10 @@ void setup()
   pinMode(PinReset, INPUT_PULLUP);
 
   lcd.begin(20, 4);
-  lcd.clear();
+  lcd.createChar(1, SymbolAverage);
 
+  ResetLcd();
+ 
   Serial.begin(112500);
 }
 
@@ -41,6 +58,8 @@ void loop()
   {
     Reset(&A);
     Reset(&B);
+    ResetLcd();
+    isReset = true;
   }
 
   if (digitalRead(PinLapA) == LOW)
@@ -51,10 +70,62 @@ void loop()
 
   auto now = millis();
 
-  lcd.setCursor(0, 0);
-  lcd.print("24 123.45  25  17,23");
-  lcd.setCursor(0, 1);
-  lcd.print("12 111.10   9  17,23");
+  if (!A.Started && !B.Started)
+    return;
+  
+  if (isReset)
+  {
+    lcd.clear();
+    isReset = false;
+  }
+
+  PrintRace(&A, now, 0);
+  PrintRace(&B, now, 11);
+} 
+
+void PrintRace(RaceInfo *info, unsigned long now, uint8_t col)
+{
+
+  if (info->Started)
+  {
+    lcd.setCursor(3 + col, 0);
+    auto elapsed = now - info->LapStart;
+    if (info->CurrentLap > 1 && elapsed < 5000)
+      PrintTime(info->LastTime, 2);
+    else
+    {
+      PrintTime(elapsed, 1);
+      lcd.print(' ');
+    }
+  }
+
+  if (!info->LapUpdate)
+    return;
+
+  lcd.setCursor(0 + col, 0);
+  PrintDigits(info->CurrentLap, 2);
+
+  lcd.setCursor(3 + col, 1);
+  PrintTime(info->LastTime, 2);
+
+  if (info->CurrentLap > 2)
+  {
+    lcd.setCursor(0 + col, 2);
+    PrintDigits(info->BestLap, 2);
+    
+    lcd.setCursor(3 + col, 2);
+    PrintTime(info->BestTime, 2);
+
+    lcd.setCursor(3 + col, 3);
+    PrintTime(info->AverageTime, 2);
+
+    lcd.setCursor(10, 2);
+    lcd.print('*');
+    lcd.setCursor(10, 3);
+    lcd.write(byte(1));
+  }
+
+  info->LapUpdate = false;
 }
 
 void Reset(RaceInfo *info)
@@ -62,9 +133,30 @@ void Reset(RaceInfo *info)
   info->Started = false;
   info->LapStart = 0;
   info->CurrentLap = 1;
-  info->LastLapTime = 0;
+  info->LastTime = 0;
   info->BestLap = 0;
-  info->BestLapTime = 0;
+  info->BestTime = 0;
+  info->LapUpdate = false;
+  info->SumTime = 0;
+  info->AverageTime = 0;
+}
+
+void ResetLcd()
+{
+  lcd.clear();
+  lcd.setCursor(2,1);
+  lcd.write((byte)0x7E);
+  lcd.write((byte)0x3E);
+  lcd.print("    Race     ");
+  lcd.write((byte)0x3C);
+  lcd.write((byte)0x7F);
+
+  lcd.setCursor(2,2);
+  lcd.write((byte)0x7E);
+  lcd.write((byte)0x3E);
+  lcd.print("    Timer    ");
+  lcd.write((byte)0x3C);
+  lcd.write((byte)0x7F);
 }
 
 void SetLap(RaceInfo *info)
@@ -79,41 +171,38 @@ void SetLap(RaceInfo *info)
   }
 
   // Debounce
-  if (now - info->LapStart < 100) 
+  if (now - info->LapStart < 500) 
     return; 
 
   auto lapTime = now - info->LapStart;
 
-  info->LastLapTime = lapTime;
-  if (lapTime < info->BestLapTime)
+  info->LastTime = lapTime;
+  if (info->BestLap == 0 || lapTime < info->BestTime)
   {
     info->BestLap = info->CurrentLap;
-    info->BestLapTime = lapTime;
+    info->BestTime = lapTime;
   }
+  info->SumTime+=lapTime;
+  info->AverageTime = info->SumTime / info->CurrentLap;
   info->LapStart = now;
   info->CurrentLap++;
+  info->LapUpdate = true;
 }
 
 void PrintTime(unsigned long ms)
 {
-  PrintTime(ms, 3);
+  PrintTime(ms, 2);
 }
 
 void PrintTime(unsigned long ms, byte fractionDigits)
 {
-  auto minutes = ms / 60000;
-  auto seconds = ms % 60000 / 1000;
+  auto seconds = ms / 1000;
   auto fraction = ms % 1000;
 
-  if (minutes > 0)
-  {
-    PrintDigits(minutes, 2, false);
-    lcd.print(':');
-  }
+  if (seconds >= 1000)
+    lcd.print("???");
   else
-    lcd.print("   ");
-
-  PrintDigits(seconds, 2, minutes > 0);
+    PrintDigits(seconds, 3, false);
 
   if (fractionDigits > 0)
     lcd.print(".");
@@ -134,7 +223,12 @@ void PrintTime(unsigned long ms, byte fractionDigits)
   }
 }
 
-void PrintDigits(int value, size_t digits, bool printZero)
+void PrintDigits(int value, byte digits)
+{
+  PrintDigits(value, digits, false);
+}
+
+void PrintDigits(int value, byte digits, bool printZero)
 {
   if (value < 100 && digits > 2)
     lcd.print(printZero ? '0' : ' ');
@@ -155,3 +249,5 @@ void PrintDigits(int value, size_t digits, bool printZero)
   }
   lcd.print(value);
 }
+
+
